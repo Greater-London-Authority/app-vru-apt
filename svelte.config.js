856f62d2a1d128fdf5283b70a6730e0c45590c05
Micheal Slade <50627681+Micheal-Slade@users.noreply.github.com>
2025-06-04 14:47:31 +0100

    Update svelte.config.js
    Adding app name

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
 	kit: {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({ fallback: undefined }),
-		paths: { base: dev ? '' : '/!!INSERT YOUR APP NAME HERE!!' }
+		paths: { base: dev ? '' : '/vru-apt' }
 	}
 };
 
